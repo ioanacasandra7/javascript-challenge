a9c0d0bac04a7314d2439a1689b72dbcb3926980
ioanacasandra7 <ioanacasandraH@gmail.com>
2020-09-19 09:15:27 -0500

    created folder for ufo-1

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,46 @@
+// from data.js
+var ufoFinder = data;
+​
+tbody = d3.select("tbody");
+​
+function createTable(Findings) {
+    data.forEach((Findings) => {
+        var row = tbody.append("tr");
+        Object.values(Findings).forEach((value) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });   
+}
+​
+createTable(data);
+​
+var button = d3.select("#filter-btn");
+​
+// Select the form
+var form = d3.select(".filters");
+​
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+​
+​
+// Complete the event handler function for the form
+function runEnter() {
+​
+    // remove previous data from the list to
+    tbody.html("");
+​
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+​
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+​
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+​
+    var filteredData = data.filter(Findings=> Findings.datetime === inputValue);
+​
+    createTable(filteredData);
+}
